d6a0a6b472746c2c4e31fbc5e254e61b9ba6dcc5
Raylson Alves Marques <raylson.marques@foursys.com.br>
2024-03-01 15:53:01 -0300

    FEATURE: Cria estrutura inicial da página inicial do site.

--- apps/root/src/app/app.routes.ts
+++ apps/root/src/app/app.routes.ts
@@ -3,12 +3,21 @@ import { LotoTemplateComponent, RouterEnum, ScreenNameEnum } from '@loto/shared'
 
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Route[] = [
 	{
 		path: RouterEnum.ROOT,
 		component: LotoTemplateComponent,
 		children: [
+			//- Public routes
+			{
+				path: RouterEnum.HOME,
+				component: HomeComponent,
+				data: {
+					name: ScreenNameEnum.INICIO
+				}
+			},
 			{
 				path: RouterEnum.SIGN_UP,
 				component: SignUpComponent,
--- libs/shared/src/lib/enums/ScreenNameEnum.ts
+++ libs/shared/src/lib/enums/ScreenNameEnum.ts
@@ -1,5 +1,6 @@
 export enum ScreenNameEnum {
 	ROOT = "ROOT",
+	INICIO = "INICIO",
 	SIGN_UP = "SIGN_UP",
 	SIGN_IN = "SIGN_IN",
 }
